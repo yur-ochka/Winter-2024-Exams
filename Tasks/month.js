5a9f73dfb247760cdb37028125515999c7ef3377
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-13 23:40:25 +0200

    Removed unnecessary variable

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,8 +2,7 @@
 
 const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 const Month = (s) => {
-  l = Months.length;
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < Months.length; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
