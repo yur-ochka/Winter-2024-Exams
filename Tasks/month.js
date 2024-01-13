c4c368ec11d60c69b4412760ea54c7a541693c1e
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-13 23:41:46 +0200

    Fixed naming

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,11 +1,11 @@
 // Get month number
 
 const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-const Month = (s) => {
+const month = (nameOfMonth) => {
   for (let i = 0; i < Months.length; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
+    if (nameOfMonth.toLowerCase().startsWith(Months[i])) return i + 1;
   }
   return -1;
 };
 
-module.exports = Month;
+module.exports = month;
