fe0112dc8af630ed6d8f6e04eb7c6f9525328a48
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-13 23:39:21 +0200

    Added "const" type for array and function

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,7 +1,7 @@
 // Get month number
 
-Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-Month = (s) => {
+const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+const Month = (s) => {
   l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
