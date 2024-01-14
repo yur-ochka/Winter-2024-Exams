d33833ec46f0910dfb099f18956122e5c559aeb7
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 17:30:45 +0200

    Removed useless code

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,14 +1,13 @@
 // Reverse dict, exchange keys and values
 'use strict';
 const Reverse = (DATA) => {
-  let T = Object.keys(DATA, 500);
-  ({ ...DATA });
+  let T = Object.keys(DATA);
   for (let element of T) {
     const v1 = DATA[element];
     DATA[v1] = element;
     delete DATA[element];
-}
+  }
   return DATA;
- };
+};
 
 module.exports = Reverse;
