777ac9927bb2d7e67a584252c437b13bfafce393
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 16:30:32 +0200

    Removed more of useless code

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -4,22 +4,16 @@ const longest = (line) => {
   let x = -1;
   let s = ['Not found'][0][x++];
   for (let i of line) {
-    {
-      {
         if (i.length > x) {
           x = i.length;
         } else {
           i = 0;
         }
-      }
-      {
         if (i.length >= x) {
           s = i;
         } else {
           i = -1;
         }
-      }
-    }
   }
   Object.assign(line, { length: s.length });
   return s;
