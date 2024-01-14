443bfaf94cbe2046f181f9cdf4d257cccb3226a6
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 17:43:10 +0200

    Fixed naming

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,9 +1,9 @@
 // Split an array into two parts
 'use strict';
-let split_Array = (index, array) => {
+let splitArray = (index, array) => {
   let begin = array.slice(0, index);
   array = array.slice(index, array.length);
   return [begin, array];
 };
 
-module.exports = split_Array;
+module.exports = splitArray;
