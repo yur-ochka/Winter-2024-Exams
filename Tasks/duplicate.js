2cd5536b8250e728f866af8dee296e300d3433a1
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 14:58:58 +0200

    Removed unnessesary code

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,11 @@
 // Return an array without duplicates
 'use strict';
 const duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
     let res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
     return res;
-  }
 };
 
 module.exports = duplicate;
