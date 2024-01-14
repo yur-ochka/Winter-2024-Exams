7e0d25cc8642046c77a189f851b87e94ab390d5c
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 17:22:04 +0200

    Removed unnessesary code

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,12 +1,7 @@
 // Generate random integer value in given range
 'use strict';
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
     return min + Math.floor(Math.random() * (max - min + 1));
-  }
 };
 
 module.exports = random;
