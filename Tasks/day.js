a060f4532880229cae80a07da12f976daf84da3c
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 14:36:02 +0200

    Improved formatting, fixed naming for constant

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,12 +1,10 @@
 // Get day number
 'use strict';
-let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (nameOfDay) => {
-  for (let i = 0; i < days.length; i++) {
-    if (nameOfDay.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
+  for (let i = 0; i < DAYS.length; i++) {
+    if (nameOfDay.startsWith(DAYS[i].toLowerCase())) return i + 1;
   }
   return -1;
 };
