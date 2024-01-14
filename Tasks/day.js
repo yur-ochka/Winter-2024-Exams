02375fa4d007bed0f7e7085487cd2feaeaf484e8
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 14:28:57 +0200

    Fixed naming, added identifier for method

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,15 @@
 // Get day number
 'use strict';
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const parseDay = (nameOfDay) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (nameOfDay.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
