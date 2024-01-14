99c188982fbadb3b38e90b96e587adb8f0d7e398
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 17:41:43 +0200

    Added key word

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,7 +1,7 @@
 // Split an array into two parts
 'use strict';
 let split_Array = (index, array) => {
-  begin = array.slice(0, index);
+  let begin = array.slice(0, index);
   const len = array.length;
   array = array.slice(index, len);
   return [begin, array];
