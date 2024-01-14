7cf0a53fc58736b941460645d95c7814d7a2c999
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-14 16:44:55 +0200

    Removed extra constant

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,10 +1,9 @@
 // Generate random password
 'use strict';
 const generatePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
   let password = '';
   for (let i = 0; i < length; i++) {
-    let index = Math.floor(Math.random() * MAX);
+    let index = Math.floor(Math.random() * alphabet.length);
     password = password + alphabet[index];
   }
   return password;
