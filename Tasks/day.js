94ca7a1782689ca485192be7c12a81aa4b286d60
Yurchenko Illia <iliayurchenko06@gmail.com>
2024-01-16 16:17:42 +0200

    Moved loop invariant out and decomposed long expression with intermediate identifiers

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,8 +3,10 @@
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (nameOfDay) => {
-  for (let i = 0; i < DAYS.length; i++) {
-    if (nameOfDay.startsWith(DAYS[i].toLowerCase())) return i + 1;
+  const lengthOfDays = DAYS.length;
+  for (let i = 0; i < lengthOfDays; i++) {
+    const dayInLowerCase = DAYS[i].toLowerCase();
+    if (nameOfDay.startsWith(dayInLowerCase)) return i + 1;
   }
   return -1;
 };
